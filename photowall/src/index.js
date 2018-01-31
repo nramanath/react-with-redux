9ffea8ea809c3db82f043aad09e8a1ca98b765c5
Ramanathan Nachiappan <nramssn@gmail.com>
2018-01-30 19:57:50 -0700

    [4-react-intro] - Creating and rendering a basic element in react

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -1,3 +1,11 @@
 // this is the entry point of the app
 // after creating the boiler plate code, remove all files in src folder except index.js
 // remove all code in index.js
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = React.createElement('h1', null, 'Hello World');
+
+ReactDOM.render(element, document.getElementById('root')); // the root element is parent div tag created in the index.html file
