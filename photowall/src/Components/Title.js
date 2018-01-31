b1e472d25017085575fe31922a082b927ab92e30
Ramanathan Nachiappan <nramssn@gmail.com>
2018-01-31 14:13:12 -0700

    [4-react-intro] - Refactoring the classes into files and organising them

---
+++ photowall/src/Components/Title.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ramanathan on 31/01/18.
+ */
+
+import React, {Component} from 'react';
+
+class Title extends Component {
+    render() {
+        return (
+            <h1> {this.props.title} </h1>
+        );
+    }
+}
+
+export default Title;
--- photowall/src/index.js
+++ photowall/src/index.js
@@ -5,35 +5,6 @@
 
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Main from './Components/Main';
 
-
-class List extends Component {
-    render() {
-        return (
-            <ol>
-                {this.props.tasks.map((task, index) => <li key={index}> {task} </li>)}
-            </ol>
-        );
-    }
-}
-
-class Title extends Component {
-    render() {
-        return (
-            <h1> {this.props.title} </h1>
-        );
-    }
-}
-
-class Main extends Component {
-    render() {
-        return (
-            <div>
-                <Title title = {'To Do List'}/>
-                <List tasks={['Do CySIS Lab Work', 'Apply for AMEX Card']}/>
-                <List tasks={['Correct the question paper', 'Order the Gift']}/>
-            </div>
-        )
-    }
-}
 ReactDOM.render(<Main/>, document.getElementById('root')); // the root element is parent div tag created in the index.html file
