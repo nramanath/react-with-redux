a1fa7887a0a8dd82917301ee8fd5106accdc2417
Ramanathan Nachiappan <nramssn@gmail.com>
2018-01-31 14:03:14 -0700

    [4-react-intro] - Changing the JSX code into components

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -3,20 +3,39 @@
 // remove all code in index.js
 
 
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 const tasks = ['Take out the trash', 'Walk the Dog', 'Bathe the baby'];
 
 
-// JSX expression - enclose it in the curly brances {}
-const element =
-    <div>
-        <h1> Task List </h1>
-        <ol>
-            {tasks.map((task, index) => <li key={index}> {task} </li>)}
-        </ol>
-    </div>
+class List extends Component {
+    render() {
+        return (
+            <ol>
+                {tasks.map((task, index) => <li key={index}> {task} </li>)}
+            </ol>
+        );
+    }
+}
 
+class Title extends Component {
+    render() {
+        return (
+            <h1> Task List </h1>
+        );
+    }
+}
 
-ReactDOM.render(element, document.getElementById('root')); // the root element is parent div tag created in the index.html file
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Title/>
+                <List/>
+                <List/>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<Main/>, document.getElementById('root')); // the root element is parent div tag created in the index.html file
