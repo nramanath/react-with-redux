23d2d458c7f13875fdd1663a7cef5c5aa9c6042b
Ramanathan Nachiappan <nramssn@gmail.com>
2018-01-30 20:00:30 -0700

    [4-react-intro] - nesting elements in creating and rendering section in react

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -6,6 +6,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = React.createElement('h1', null, 'Hello World');
+const element = React.createElement('ol', null,
+    React.createElement('li', null, 'Take out the trash'),
+    React.createElement('li', null, 'Shovel the driveway'),
+    React.createElement('li', null, 'Walk the dog'),
+);
 
 ReactDOM.render(element, document.getElementById('root')); // the root element is parent div tag created in the index.html file
