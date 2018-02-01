510d36aef66c537df84030e97c557f11c222e7de
Ramanathan Nachiappan <nramssn@gmail.com>
2018-02-01 11:16:35 -0700

    [5-react-state-management] - linking style sheet to the pages

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -6,5 +6,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main';
+import './styles/stylesheet.css';
 
 ReactDOM.render(<Main/>, document.getElementById('root')); // the root element is parent div tag created in the index.html file
