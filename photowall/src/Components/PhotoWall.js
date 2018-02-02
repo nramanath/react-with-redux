72c8b8a22691690e2bc1b0c01235709c57c0c32a
Ramanathan Nachiappan <nramssn@gmail.com>
2018-02-02 15:23:47 -0700

    [5-react-state-management] - Updating state by deleting a photo, also explains this binding to the function of the class during event handling

--- photowall/src/Components/PhotoWall.js
+++ photowall/src/Components/PhotoWall.js
@@ -9,7 +9,7 @@ import Photo from "./Photo";
 function PhotoWall(props) {
     return (
         <div className="photoGrid">
-            {props.posts.map((post, index) => <Photo post={post} key={index}/>)}
+            {props.posts.map((post, index) => <Photo post={post} key={index} onRemovePhoto={props.onRemovePhoto}/>)}
         </div>
     )
 }
