6d3bd787db7e3deca61a3d5cba3c9db4fac5bc30
Ramanathan Nachiappan <nramssn@gmail.com>
2018-02-02 14:57:47 -0700

    [5-react-state-management] - Rule of thumb for converting class components to functional components

--- photowall/src/Components/Title.js
+++ photowall/src/Components/Title.js
@@ -5,6 +5,11 @@
 import React, {Component} from 'react';
 
 
+// the rule of thumb - if we are using only the render method of the class, then we can
+// convert them into stateless functional components
+
+// however, we usually use class components, since we have many uses of the lifecycle of the classes
+
 function Title(props) {
     return (
         <h1> {props.title} </h1>
